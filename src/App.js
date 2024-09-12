91b0a38a3de7fd62a424c44db86e5f35232878fc
benjaminhernandez1981 <benjamineric2005@gmail.com>
2024-09-11 19:40:36 -0500

    firebase-practice

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
+import React from "react";
 import "./App.css";
 import { auth } from "./firebase/init";
 import {
   createUserWithEmailAndPassword,
   signInWithEmailAndPassword,
+  signOut,
+  onAuthStateChanged,
 } from "firebase/auth";
 function App() {
+  const [user, setUser] = React.useState({});
+  const [loading, setLoading] = React.useState(true);
+
+  React.useEffect(() => {
+    onAuthStateChanged(auth, (user) => {
+      setLoading(false);
+      console.log(user);
+      if (user) {
+        setUser(user);
+      }
+    });
+  }, []);
+
   function register() {
     console.log("register");
     createUserWithEmailAndPassword(
@@ -26,18 +42,26 @@ function App() {
       "email5678!!!45324456@email.com",
       "test123"
     )
-      .then((user) => {
+      .then(({ user }) => {
         console.log(user);
+        setUser(user);
       })
       .catch((error) => {
-        console.log(error);
+        console.log(error.message);
       });
+  }
+
+  function logout() {
+    signOut(auth);
+    setUser({});
   }
 
   return (
     <div className="App">
       <button onClick={register}>Register</button>
       <button onClick={login}>Login</button>
+      <button onClick={logout}>Logout</button>
+      {loading ? "loading..." : user.email}
     </div>
   );
 }
